8aed87f1c0bce0ae3edac4098e9562ed3b45eaff
Avi Varma <avi.varma@ymail.com>
2022-05-23 21:58:35 +0100

    My powershell profile.

---
+++ PowerShell Profile/Avi.ps1
@@ -0,0 +1,12 @@
+# Icons
+
+Import-Module -Name Terminal-Icons
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
