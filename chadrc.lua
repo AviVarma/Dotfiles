f78f865405b7950b6dfeb2c9dc42e845a018a3e6
Avi Varma <avi.varma@ymail.com>
2022-05-23 21:49:54 +0100

    Updated to what I use on nvchad.

---
+++ chadrc.lua
@@ -0,0 +1,23 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+-- remove this if you dont use custom.init.lua at all
+require "custom"
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+   theme = "tokyodark",
+}
+
+M.plugins = {
+  user = {
+    ["goolord/alpha-nvim"] = {
+      disable = false,
+    },
+  },
+}
+
+return M
--- init.vim
+++
@@ -1,6 +0,0 @@
-"Nvim prefrences...
-set number
-set autoindent smartindent
-set tabstop=4
-set softtabstop=0 noexpandtab
-set shiftwidth=4
