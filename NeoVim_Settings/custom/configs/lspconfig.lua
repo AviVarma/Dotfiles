47cbbdd2ef3a6441f318e975a30ff25b96e4954f
Avi Varma <avi.varma@ymail.com>
2023-03-19 21:33:29 +0000

    Added language servers for all the stuff I use.
    Rust analyzer is now working just like it does on VS-Code.

--- NeoVim_Settings/custom/configs/lspconfig.lua
+++ NeoVim_Settings/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer" , "robotframework-lsp", "ruff-lsp"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
